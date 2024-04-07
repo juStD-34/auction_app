e59f12d4254d0e09b2aa7afdbd89709471a10e71
Phạm Minh Hiếu 21020319 <21020319@vnu.edu.vn>
2024-04-07 17:28:44 +0700

    feat: Complete Register + validation

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./home/Home"
+import SignUp from "./home/signup/SignUp";
 import Buyer from "./buyer/Buyer"
 import Seller from "./seller/Seller"
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/register" element={<SignUp />} />
         <Route path="/buyer" element={<Buyer />} />
         <Route path="/seller" element={<Seller />} />
       </Routes>
