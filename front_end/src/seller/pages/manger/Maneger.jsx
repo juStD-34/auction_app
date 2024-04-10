41101b229118cc0c9d11cbf8ca0c8e8d8f733dfd
21020313 - Nguyen Dang Hai <21020313@vnu.edu.vn>
2024-04-10 23:29:59 +0700

    Upload Seller

---
+++ front_end/src/seller/pages/manger/Maneger.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
+
+export default function Maneger() {
+    return (
+        <div className='flex'>
+            <Sidebar />
+
+            {/* Code ngoài Sidebar */}
+            <div className="h-screen flex-1 p-7">
+                <h1 class="mb-4 text-1xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-3xl">QUẢN LÝ PHIÊN ĐẤU GIÁ</h1>
+                <section class="bg-gray-50 light:bg-gray-900 py-3 sm:py-5">
+                    <div class="px-4 mx-auto max-w-screen-2xl lg:px-12">
+                        <div class="relative overflow-hidden bg-white shadow-md light:bg-gray-800 sm:rounded-lg">
+                            <div class="flex flex-col px-4 py-3 space-y-3 lg:flex-row lg:items-center lg:justify-between lg:space-y-0 lg:space-x-4">
+                            </div>
+                            <div class="overflow-x-auto">
+                                <table class="w-full text-sm text-left text-gray-500 light:text-gray-400">
+                                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 light:bg-gray-700 light:text-gray-400 border-b light:border-neutral-600">
+                                        <tr>
+                                            <th scope="col" class="p-4">
+                                                <div class="flex items-center">
+                                                    <label htmlFor="">STT</label>
+                                                </div>
+                                            </th>
+                                            <th scope="col" class="px-4 py-3 border-x light:border-neutral-600">Tên sản phẩm</th>
+                                            <th scope="col" class="px-4 py-3 border-x light:border-neutral-600">ID</th>
+                                            <th scope="col" class="px-4 py-3 border-x light:border-neutral-600">Thời gian đấu giá còn lại</th>
+                                            <th scope="col" class="px-4 py-3 border-x light:border-neutral-600">Giá khởi điểm</th>
+                                            <th scope="col" class="px-4 py-3 border-x light:border-neutral-600">Giá hiện tại</th>
+                                            <th scope="col" class="px-4 py-3 border-x light:border-neutral-600">Người đấu giá cao nhất</th>
+                                            <th scope="col" class="px-4 py-3 border-x light:border-neutral-600">Dừng đấu giá ngay</th>
+                                        </tr>
+                                    </thead>
+
+                                    {/* DATA mẫu */}
+
+                                    <tbody>
+                                        <tr class="border-b light:border-gray-600 hover:bg-gray-100 light:hover:bg-gray-700 border-b light:border-neutral-600">
+                                            <td class="w-4 px-4 py-3">
+                                                <div class="flex items-center">
+                                                    <label htmlFor="">1</label>
+                                                </div>
+                                            </td>
+                                            <td scope="row" class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap light:text-white border-x light:border-neutral-600">
+                                                Monitor BenQ EX2710Q
+                                            </td>
+                                            <td class="px-4 py-2">
+                                                <span class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap light:text-white">12345</span>
+                                            </td>
+                                            <td class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap light:text-white border-x light:border-neutral-600">
+                                                <div class="ml-1 text-gray-500 light:text-gray-400">
+                                                    <label className='time'>11:20</label>
+                                                </div>
+                                            </td>
+                                            <td class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap light:text-white border-x light:border-neutral-600">140000</td>
+                                            <td class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap light:text-white border-x light:border-neutral-600">160000</td>
+                                            <td class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap light:text-white border-x light:border-neutral-600">
+                                                <div class="flex items-center">
+
+                                                    <span class="ml-1 text-gray-500 light:text-gray-400">Nguyễn Văn A</span>
+                                                </div>
+                                            </td>
+                                            <td class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap light:text-white border-x light:border-neutral-600">
+                                                <div class="flex items-center">
+                                                    <button type="button" class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Dừng đấu giá</button>
+                                                </div>
+                                            </td>
+
+                                        </tr>
+                                    </tbody>
+
+                                    {/* Hết Mẫu */}
+
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+            </div>
+            {/* END */}
+        </div>
+    )
+}
---
+++ front_end/src/seller/pages/sell_page/SellPage.jsx
@@ -0,0 +1,150 @@
+import React, { useState } from "react";
+import Sidebar from "../../components/sidebar/Sidebar"
+
+export default function SellPage() {
+  const [selectedImages, setSelectedImages] = useState([]);
+
+  const onSelectFile = (event) => {
+    const selectedFiles = event.target.files;
+    const selectedFilesArray = Array.from(selectedFiles);
+
+    const imagesArray = selectedFilesArray.map((file) => {
+      return URL.createObjectURL(file);
+    });
+
+    setSelectedImages((previousImages) => previousImages.concat(imagesArray));
+
+    // FOR BUG IN CHROME
+    event.target.value = "";
+  };
+
+  function deleteHandler(image) {
+    setSelectedImages(selectedImages.filter((e) => e !== image));
+    URL.revokeObjectURL(image);
+  }
+
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="h-screen flex-1 p-7">
+        <h1 class="mb-4 text-1xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-3xl">TẠO PHIÊN ĐẤU GIÁ MỚI</h1>
+
+        {/* Điền */}
+
+        <form>
+          <div className="grid gap-6 mb-6 md:grid-cols-2">
+            <div>
+              <label for="text" class="block text-sm font-semibold mb-2">Tên sản phẩm:</label>
+              <input type="text" id="text" class="w-full p-2 border border-gray-300 rounded " placeholder="Tên sản phẩm" required />
+            </div>
+            <div>
+              <label for="text" class="block text-sm font-semibold mb-2">Loại tài sản đấu giá:</label>
+              <select id="default" class="border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
+                <option selected>Chọn loại sản phẩm đấu giá</option>
+                <option value="US">Tài sản nhà nước</option>
+                <option value="CA">Tài sản bất động sản</option>
+                <option value="FR">Phương tiện xe cộ</option>
+                <option value="DE">Văn hóa nghệ thuật</option>
+              </select>
+            </div>
+
+          </div>
+          <div class="mb-6">
+            <label for="message" class="block text-sm font-semibold mb-2">Mô tả sản phẩm:</label>
+            <textarea id="message" rows="2" class="w-full p-2 border border-gray-300 rounded" placeholder="Viết mô tả của bạn ở đây..." required></textarea>
+          </div>
+          <div className="grid gap-6 mb-6 md:grid-cols-4">
+            <div>
+              <label className="block text-sm font-semibold mb-2">Giá khởi điểm:</label>
+              <div class="relative mb-4 flex w-full flex-wrap items-stretch">
+                <input
+                  type="number"
+                  class="relative m-0 block flex-auto border border-solid border-neutral-200 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-surface outline-none transition duration-200 ease-in-out placeholder:text-neutral-500 focus:z-[3] focus:border-primary focus:shadow-inset focus:outline-none motion-reduce:transition-none"
+                  aria-label="Amount (to the nearest dollar)" />
+                <span
+                  class="flex items-center whitespace-nowrap rounded-e border border-s-0 border-solid border-neutral-200 px-3 py-[0.25rem] text-center text-base font-normal leading-[1.6] text-surface"
+                >VND</span
+                >
+              </div>
+            </div>
+          </div>
+
+          {/*Upload Image */}
+
+          <div class="mb-6">
+            <label class="block mb-2 text-sm font-medium text-gray-900" for="multiple_files">Đăng ảnh sản phẩm:</label>
+            <div className="grid gap-6 mb-6 md:grid-cols-4">
+              <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none"
+                id="multiple_files"
+                type="file"
+                name="images"
+                onChange={onSelectFile}
+                multiple
+                accept="image/png , image/jpeg, image/webp, image/jpg"
+                required />
+            </div>
+            <p class="mt-1 text-sm text-gray-500" id="file_input_help"> Chấp nhận ảnh PNG, JPEG, JPG hoặc WEBP.</p>
+
+            {selectedImages.length > 0 &&
+               (
+                <button
+                  className="upload-btn text-red"
+                  onClick={() => {
+                    console.log(selectedImages);
+                  }}
+                >
+                  Đã tải lên {selectedImages.length} ảnh
+                </button>
+              )}
+
+            <div className="images" class="grid gap-6 mb-6 md:grid-cols-6">
+              {selectedImages &&
+                selectedImages.map((image, index) => {
+                  return (
+                    <div key={image} className="image">
+                      <img class="object-fill h-48 w-96" src={image} height="10px" alt="upload" />
+                      <hr />
+                      <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                        onClick={() => deleteHandler(image)}>
+                        Xóa ảnh
+                      </button>
+                    </div>
+                  );
+                })}
+            </div>
+          </div>
+
+          {/* Ngày */}
+
+          <div class="mb-6">
+            <div className="grid gap-6 mb-6 md:grid-cols-4">
+              <div>
+                <label className="block text-sm font-semibold mb-2">Thời gian bắt đầu:</label>
+                <input type="time" className="w-full p-2 border border-gray-300 rounded" />
+              </div>
+              <div>
+                <label className="block text-sm font-semibold mb-2"> Ngày bắt đầu:</label>
+                <input type="date" className="w-full p-2 border border-gray-300 rounded" />
+              </div>
+            </div>
+            <div className="grid gap-6 mb-6 md:grid-cols-4">
+              <div>
+                <label className="block text-sm font-semibold mb-2">Thời gian kết thúc:</label>
+                <input type="time" className="w-full p-2 border border-gray-300 rounded" />
+              </div>
+              <div>
+                <label className="block text-sm font-semibold mb-2"> Ngày kết thúc:</label>
+                <input type="date" className="w-full p-2 border border-gray-300 rounded" />
+              </div>
+            </div>
+          </div>
+          {/* Nút bấm */}
+          <div class="grid gap-6 mb-6 md:grid-cols-8">
+            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Tạo đấu giá</button>
+            <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Hủy</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
