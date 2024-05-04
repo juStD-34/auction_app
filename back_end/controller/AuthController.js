0991073fa427543a2ee65efa42b7ffe6321ed284
Đặng Trí Nhân <21020371@vnu.edu.vn>
2024-05-04 15:08:01 +0700

    new commit

--- back_end/controller/AuthController.js
+++ back_end/controller/AuthController.js
@@ -3,13 +3,13 @@ const { createSecretToken } = require("../util/SecretToken");
 const bcrypt = require("bcryptjs");
 
 module.exports.Signup = async (req, res, next) => {
+  const { email, password, username, role } = req.body;
   try {
-    const { email, password, username, createdAt } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.json({ message: "User already exists" });
     }
-    const user = await User.create({ email, password, username, createdAt });
+    const user = await User.create({ email, password, username, role });
     const token = createSecretToken(user._id);
     res.cookie("token", token, {
       withCredentials: true,
@@ -25,32 +25,32 @@ module.exports.Signup = async (req, res, next) => {
 }
 
 module.exports.Login = async (req, res, next) => {
+  const { email, password } = req.body;
   try {
-    const { email, password } = req.body;
-    if(!email || !password ){
-      return res.json({message:'All fields are required'})
+    if (!email || !password) {
+      return res.json({ message: 'All fields are required' })
     }
     const user = await User.findOne({ email });
-    if(!user){
-      return res.json({message:'Incorrect password or email' }) 
+    if (!user) {
+      return res.json({ message: 'Incorrect password or email' })
     }
-    const auth = await bcrypt.compare(password,user.password)
+    const auth = await bcrypt.compare(password, user.password)
     if (!auth) {
-      return res.json({message:'Incorrect password or email' }) 
+      return res.json({ message: 'Incorrect password or email' })
     }
-     const token = createSecretToken(user._id);
-     res.cookie("token", token, {
+    const token = createSecretToken(user._id);
+    res.cookie("token", token, {
       httpOnly: true, // Set to true for security
       secure: process.env.NODE_ENV === 'production', // Use secure in production
       sameSite: 'strict' // SameSite setting
-  });
-     res.status(201).json({ 
-        message: "User logged in successfully",
-        success: true,
-        token: token,
-        user: user
-     });
-     next()
+    });
+    res.status(201).json({
+      message: "User logged in successfully",
+      success: true,
+      token: token,
+      user: user
+    });
+    next()
   } catch (error) {
     console.error(error);
   }
