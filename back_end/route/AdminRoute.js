4cae02200b5faa506a0479cbe20462b1882b2256
Đặng Trí Nhân <21020371@vnu.edu.vn>
2024-05-05 14:55:28 +0700

    new commit

--- back_end/route/AdminRoute.js
+++ back_end/route/AdminRoute.js
@@ -3,15 +3,7 @@ const adminRouter = require("express").Router();
 const {getIncomingAuction, getAllSeller, getAllBidder, getAllAuction} = require("../controller/AdminController");
 
 
-const getIncomingAuctionApi = "/getIncomingAuction"
-const getAllSellerApi = "/getAllSeller";
-const getAllBidderApi = "/getAllBidder";
-const getAllAuctionApi = "/getAllAuction";
 
-adminRouter.get(getIncomingAuctionApi, getIncomingAuction);
-adminRouter.get(getAllSellerApi, getAllSeller);
-adminRouter.get(getAllBidderApi, getAllBidder);
-adminRouter.get(getAllAuctionApi, getAllAuction);
 adminRouter.get('/allAuction', Admin.getAllAuction);
 adminRouter.get('/allBidder', Admin.getAllBidder);
 adminRouter.get('/allSeller', Admin.getAllSeller);
