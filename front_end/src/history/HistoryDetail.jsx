549e8b8ed4d5d8adcf110ed63c5b9b4887c43ba5
Phạm Minh Hiếu 21020319 <21020319@vnu.edu.vn>
2024-05-09 22:43:00 +0700

    history detail

--- front_end/src/history/HistoryDetail.jsx
+++ front_end/src/history/HistoryDetail.jsx
@@ -26,14 +26,27 @@ export default function HistoryDetail() {
   const datas = useAuctionId(id);
   if (datas.error) return <div>An error has occurred: {datas.error.message}</div>;
   if (datas.isLoading) return <div>Loading...</div>;
-  console.log(datas.auction);
+  console.log(datas.auction.existAuction);
+  const data = datas.auction.existAuction;
+
+  function formatDate(dateString) {
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    };
+    return new Date(dateString).toLocaleDateString("vi-VN", options);
+  }
   
   return (
     <>
       <NavbarUser />
       <div class="mx-16">
         <div className="border-b-2 py-8 bg-[url('https://lacvietauction.vn/LVT/assets/images/bg/client-right.png')]">
-          <p className="text-3xl font-semibold pl-8">{id}</p>
+          <p className="text-3xl font-semibold pl-8">{data.name}</p>
         </div>
         <div className="ml-16 my-10 grid grid-cols-5 gap-4 content-center">
           <img
@@ -44,19 +57,19 @@ export default function HistoryDetail() {
           <div className="col-span-2 p-4 ml-8 border rounded-lg border-gray-300">
             <div className="flex mx-1 mt-2">
               <p className="text-gray-700">Mã tài sản:</p>
-              <p className="self-end ml-auto text-red-700 font-semibold">{obj.title}</p>
+              <p className="self-end ml-auto text-red-700 font-semibold">{id}</p>
             </div>
             <div className="flex mx-1 mt-2">
               <p className="text-gray-700">Thời gian bắt đầu:</p>
-              <p className="self-end ml-auto text-red-700 font-semibold">{obj.startDate}</p>
+              <p className="self-end ml-auto text-red-700 font-semibold">{formatDate(data.timeStart)}</p>
             </div>
             <div className="flex mx-1 mt-2">
               <p className="text-gray-700">Thời gian kết thúc:</p>
-              <p className="self-end ml-auto text-red-700 font-semibold">{obj.endDate}</p>
+              <p className="self-end ml-auto text-red-700 font-semibold">{formatDate(data.timeEnd)}</p>
             </div>
             <div className="flex mx-1 mt-2">
               <p className="text-gray-700">Giá khởi điểm:</p>
-              <p className="self-end ml-auto text-red-700 font-semibold">{obj.startPrice}</p>
+              <p className="self-end ml-auto text-red-700 font-semibold">{data.startPrice}</p>
             </div>
             <div className="flex mx-1 mt-2">
               <p className="text-gray-700">Giá chốt:</p>
@@ -72,11 +85,7 @@ export default function HistoryDetail() {
             </div>
             <div className="flex mx-1 mt-2">
               <p className="text-gray-700">Tên người mua</p>
-              <p className="self-end ml-auto text-red-700 font-semibold">{obj.buyer}</p>
-            </div>
-            <div className="flex mx-1 mt-2">
-              <p className="text-gray-700">Số điện thoại người mua</p>
-              <p className="self-end ml-auto text-red-700 font-semibold">{obj.phone}</p>
+              <p className="self-end ml-auto text-red-700 font-semibold">{obj.winnerID}</p>
             </div>
           </div>
         </div>
